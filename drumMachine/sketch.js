0899fb6265f7041dc54902b9a9b3fc002962189d
Bradley Johnson <bradjjohnson92@gmail.com>
2019-01-28 10:40:56 -0800

    updating files

--- drumMachine/sketch.js
+++ drumMachine/sketch.js
@@ -1,127 +1,133 @@
-/*jshint esversion: 6 */
+let recorder, soundFile, canvas;
+let prevX, prevY;
+let state = 0; // mousePress will increment from Record, to Stop, to Play
+let synth;
 
-let hh, clap, bass; // INSTRUMENT. will serve as a container that holds a sound source
-let hPat, cPat, bPat; // INSTRUMENT PATTERN. it will be an array of numbers that we can manipulate
-let hPhrase, cPhrase, bPhrase; // INSTRUMENT PHRASE. defines how the instrument pattern is interpreted
-let drums; // PART. we will attach the phrase to the part, which will serve as transport to drive phrase
-let bpmCTRL;
-let beatLength;
-let cellWidth;
-let cnv;
-let sPat;
 
 function setup() {
-  cnv = createCanvas(320, 60);
-  cnv.mousePressed(canvasPressed);
+  canvas = createCanvas(800, 800);
+  background(255);
+  fill(0);
+  strokeWeight(100);
+  text('Draw on the canvas to produce music', 20, 20);
 
-  beatLength = 16;
-  cellWidth = width / beatLength;
+  // create a sound recorder
+  recorder = new p5.SoundRecorder();
 
-  hh = loadSound('drumSamples/hh_sample.mp3', () => {});
-  clap = loadSound('drumSamples/clap_sample.mp3', () => {});
-  bass = loadSound('drumSamples/bass_sample.mp3', () => {});
+  let producedAudio = new p5.AudioIn();
+  producedAudio.setSource(0);
+  
+  synth = new p5.SinOsc();
+  recorder.setInput(synth);
 
-  hPat = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-  cPat = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  bPat = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-  sPat = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  // create an empty sound file that we will use to playback the recording
+  soundFile = new p5.SoundFile();
 
-  hPhrase = new p5.Phrase('hh', (time) => {
-    hh.play(time);
-  }, hPat);
-  cPhrase = new p5.Phrase('clap', (time) => {
-    clap.play(time);
-  }, cPat);
-  bPhrase = new p5.Phrase('bass', (time) => {
-    bass.play(time);
-  }, bPat);
+  // Button to begin recording audio
+  let startRecording = document.createElement('button')
+  startRecording.innerText = 'Start Recording';
+  startRecording.onclick  = () => {
+    recorder.record(soundFile);
+  }
+  document.body.appendChild(startRecording)
 
-  drums = new p5.Part();
+  // Button to stop recording audio
+  let stopRecording = document.createElement('button')
+  stopRecording.innerText = 'Stop Recording';
+  stopRecording.onclick  = () => {
+    recorder.stop()
+  }
+  document.body.appendChild(stopRecording)
 
-  drums.addPhrase(hPhrase);
-  drums.addPhrase(cPhrase);
-  drums.addPhrase(bPhrase);
-  drums.addPhrase('seq', sequence, sPat);
+  let play = document.createElement('button')
+  play.innerText = 'Play'
+  play.onclick = () => {
+    function getSum(total, num) {
+      return total + num;
+    }
 
-  bpmCTRL = createSlider(30, 200, 80, 1);
-  bpmCTRL.position(10, 70);
-  bpmCTRL.input(() => {
-    drums.setBPM(bpmCTRL.value());
-  });
-  drums.setBPM('80');
+    let coloredPixels = [];
+    for(let i = 0; i < 16; i++) {
+      let pixels = canvas.drawingContext.getImageData(i * 50, 0, 50, 800);
 
-  drawMatrix();
-}
-
-function keyPressed() {
-  if (key === " ") {
-    if (hh.isLoaded() && clap.isLoaded() && bass.isLoaded()) {
-      if (!drums.isPlaying) {
-        drums.metro.metroTicks = 0; // restarts playhead at beginning [0]
-        drums.loop();
-      } else {
-        drums.stop();
+      let j = 0;
+      while(j < pixels.data.length) {
+        if(pixels.data[j] < 255) {
+          coloredPixels.push(j / 800)
+        }
+        j += 4;
+      }
+      console.log('coloredPixels array is')
+      console.log(coloredPixels)
+      if(coloredPixels.length) {
+        let average = coloredPixels.reduce(getSum)/coloredPixels.length;
+        console.log('average is ')
+        console.log(average)
+        synth.start();
+        synth.fade(.5, .2);
+        synth.amp(2);
+        synth.freq(midiToFreq(((60 * average)/500) + 30));
+        setTimeout(10000);
       }
     }
   }
+  document.body.appendChild(play)
+
+  // Button to download the currently recorded audio
+  let download = document.createElement('button')
+  download.innerText = 'Download';
+  download.onclick  = () => {
+    saveSound(soundFile, 'myHorribleSound.wav');
+    // Re-initialize the soundfile
+    soundFile = new p5.SoundFile();
+    // Retrieve all pixels from the canvas
+
+
+  }
+  document.body.appendChild(download)
 }
 
-function canvasPressed() {
-  let rowClicked = floor(3 * mouseY / height);
-  let indexClicked = floor(16 * mouseX / width);
-  if (rowClicked === 0) {
-    hPat[indexClicked] = invert(hPat[indexClicked]);
-  } else if (rowClicked === 1) {
-    cPat[indexClicked] = invert(cPat[indexClicked]);
-  } else if (rowClicked === 2) {
-    bPat[indexClicked] = invert(bPat[indexClicked]);
-  }
+function mousePressed() {
+  if(state === 0) {
+    // Begin playing the synth
+    synth.start();
+    synth.fade(0.5, 0.2);
+    state++;
 
-  drawMatrix();
-}
-
-function drawMatrix() {
-  background('orange');
-  stroke('white');
-  strokeWeight(2.25);
-  fill('black');
-  for (let i = 0; i < beatLength + 1; i++) {
-    // startx, starty, endx, endy
-    line(i * cellWidth, 0, i * cellWidth, height);
-  }
-
-  for (let i = 0; i < 4; i++) {
-    line(0, i * height / 3, width, i * height / 3);
-  }
-
-  noStroke();
-
-  for (let i = 0; i < beatLength; i++) {
-    if (hPat[i] === 1) {
-      ellipse(i * cellWidth + 0.5 * cellWidth, height / 6, 10);
-    }
-    if (cPat[i] === 1) {
-      ellipse(i * cellWidth + 0.5 * cellWidth, height / 2, 10);
-    }
-    if (bPat[i] === 1) {
-      ellipse(i * cellWidth + 0.5 * cellWidth, height * 5 / 6, 10);
-    }
+    // Reset the previous mouse position
+    prevX = 0;
+    prevY = 0;
+  } else {
+    state = 0;
   }
 }
 
-function invert(bitInput) {
-  return bitInput ? 0 : 1;
+function mouseReleased() {
+    // Stop playing synth
+    synth.fade(0,0.5);
+    synth.stop();
+    state = 0;
 }
 
-function sequence(time, beatIndex) {
-  setTimeout(() => { // syncs up the timing so the beats and the playhead are in sync
-    drawMatrix();
-    drawPlayhead(beatIndex);
-  }, time * 1000);
-}
+function draw() {
+  // Set previous mouse position correctly if starting a new line
+  if(prevX === 0) {
+    prevX = mouseX;
+    prevY = mouseY;
+  }
 
-function drawPlayhead(beatIndex) {
-  stroke('blue');
-  fill(255, 0, 0, 20);
-  rect((beatIndex - 1) * cellWidth, 0, cellWidth, height);
+  if(state) {
+    // Turn up volume of synthillator tone
+    synth.amp(2);
+    // Gives us a value between 30 and  80 (good audible frequencies)
+    synth.freq(midiToFreq(((60 * mouseY)/500) + 30)); 
+
+    // Start black stroke
+    stroke(0);
+    line(prevX, prevY, mouseX, mouseY);
+
+    // Save previous mouse position for next line() call
+    prevX = mouseX;
+    prevY = mouseY;
+  }
 }
